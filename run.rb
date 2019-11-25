05d808a6d9f3a55bc053aee07f553c75f6e8a5cd
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-11-25 09:34:57 -0500

    Done.

--- run.rb
+++ run.rb
@@ -3,7 +3,10 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+zoo = Zoo.new('Atlanta Zoo', 'Atlanta, GA')
+panda = Animal.new('Panda', 275, 'Joe', zoo)
+owl = Animal.new('Owl', 8, 'the dark knight', zoo)
+anteater = Animal.new('Giant Anteater', 45, 'the sniffler', zoo)
 
 
 binding.pry
